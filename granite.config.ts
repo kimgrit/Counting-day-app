2973a6ec139b1524fc1927bb76b5f184e91b2f41
kimgrit <sungmini1102@gmail.com>
2026-03-30 22:51:05 +0900

    Update styles and viewport settings: Adjusted color scheme and background in CSS, improved body layout, and refined viewport meta tag for better responsiveness.

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: '기념일 계산기', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#725CFF', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "null", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/2223/cab8e14d-65b7-4eb1-9778-282fa5982fe0.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: '192.168.0.45',
